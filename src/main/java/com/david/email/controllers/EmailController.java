1521393641e2764baf4d064974e46581e27274c3
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-08-15 13:00:58 -0300

    Controller e configuração do e-mail

---
+++ src/main/java/com/david/email/controllers/EmailController.java
@@ -0,0 +1,30 @@
+package com.david.email.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.david.email.dtos.EmailDto;
+import com.david.email.models.EmailModel;
+import com.david.email.services.EmailService;
+
+@RestController
+public class EmailController {
+    
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/david/email/services/EmailService.java
+++ src/main/java/com/david/email/services/EmailService.java
@@ -1,8 +1,15 @@
 package com.david.email.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.david.email.enums.StatusEmail;
+import com.david.email.models.EmailModel;
 import com.david.email.repositories.EmailRepository;
 
 @Service
@@ -10,4 +17,26 @@ public class EmailService {
 
     @Autowired
     EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            
+        }
+        return emailRepository.save(emailModel);
+    }
 }
